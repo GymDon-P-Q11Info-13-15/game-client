356ae71690505fd99e9157444d1c417065c571a7
Newton34507 <Hansrudi.vonWeilheim@gmx.de>
2014-05-07 09:15:00 +0200

    Added some classes

--- Game Client/src/de/gymdon/inf1315/game/Bowman.java
+++ Game Client/src/de/gymdon/inf1315/game/Bowman.java
@@ -1,5 +1,24 @@
 package de.gymdon.inf1315.game;
 
 public class Bowman extends Unit {
+    
+    
+    @Override
+    public void move() {
+	
+	
+    }
+
+    @Override
+    public void attack() {
+	
+	
+    }
+
+    @Override
+    public void setHP(int health) {
+	
+	
+    }
 
 }
