5193775821071e6695ca09b374ee2904259bf3eb
schabeja <schabeja@10.17.1.3>
2014-05-14 08:50:00 +0200

    The archer request

---
+++ Game Client/src/de/gymdon/inf1315/game/Archer.java
@@ -0,0 +1,42 @@
+package de.gymdon.inf1315.game;
+
+public class Archer extends Unit {
+
+    public Archer(Player owner, int x, int y) {
+	this.owner = owner;
+	this.x = x;
+	this.y = y;
+	speed = 0;
+	range = 0;
+	attack = 0;
+	defense = 0;
+	hp = 0;
+    }
+
+    @Override
+    public void move(int x, int y) {
+	super.move(x, y);
+    }
+
+    @Override
+    public void attack() {
+
+	// TODO: Generate attack() method
+
+    }
+
+    @Override
+    public void setHP(int hp) {
+
+	this.hp = hp;
+
+    }
+
+    @Override
+    public int getSpeed() {
+
+	return speed;
+
+    }
+
+}
