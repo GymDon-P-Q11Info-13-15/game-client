75e6b0430eafe2d05789dc4c79c664d63b7160fa
skyrising <skyrising@pvpctutorials.de>
2014-05-22 10:32:06 +0200

    Render to texture

--- Game Client/src/de/gymdon/inf1315/game/render/GameCanvas.java
+++ Game Client/src/de/gymdon/inf1315/game/render/GameCanvas.java
@@ -1,6 +1,7 @@
 package de.gymdon.inf1315.game.render;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 import javax.swing.*;
 
@@ -14,16 +15,20 @@ public class GameCanvas extends JPanel {
 
     @Override
     public void paint(Graphics g) {
-	Graphics2D g2d = (Graphics2D) g;
-	g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-	g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 	int width = getWidth();
 	int height = getHeight();
+	BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	Graphics2D g2d = (Graphics2D)image.createGraphics();
+	g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+	g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 	g2d.setColor(Color.BLACK);
 	g2d.fillRect(0, 0, getWidth(), getHeight());
 	if (mapRenderer != null)
 	    mapRenderer.render(g2d, width, height, scrollX, scrollY);
 	if (Client.instance.currentScreen != null)
 	    Client.instance.currentScreen.render(g2d, width, height, scrollX,  scrollY);
+	g2d.dispose();
+	
+	g.drawImage(image, 0, 0, null);
     }
 }
