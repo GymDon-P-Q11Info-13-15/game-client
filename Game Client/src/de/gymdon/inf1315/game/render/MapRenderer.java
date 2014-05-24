9ad5a293c6f7841c18c0080d007855cbe1ce58ce
skyrising <skyrising@pvpctutorials.de>
2014-05-24 17:38:14 +0200

    Render to image

--- Game Client/src/de/gymdon/inf1315/game/render/MapRenderer.java
+++ Game Client/src/de/gymdon/inf1315/game/render/MapRenderer.java
@@ -4,6 +4,7 @@ import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,12 +18,15 @@ public class MapRenderer implements Renderable, ActionListener, MouseInputListen
     public List<GuiControl> controlList = new ArrayList<GuiControl>();
     protected int width;
     protected int height;
-    public int pixelSize = 32;
-    public int pixelSizeBig = pixelSize * 2;
+    public static final int TILE_SIZE_SMALL = 32;
+    public static final int TILE_SIZE_NORMAL = 64;
+    public static final int TILE_SIZE_BIG = 128;
+    public int tileSize = TILE_SIZE_SMALL;
     private MapGenerator mapgen = new MapGenerator();
+    private BufferedImage map = null;
+    private Tile[][] mapCache = null;
 
     public MapRenderer() {
-
 	mapgen.generateAll();
     }
 
@@ -34,24 +38,30 @@ public class MapRenderer implements Renderable, ActionListener, MouseInputListen
 	    c.render(g2d, width, height, scrollX, scrollY);
 
 	Tile[][] map = mapgen.getMap();
-	Building[][] buildings = mapgen.getBuildings();
-	
-	// Maps
-	for (int x = 0; x < map.length; x++) {
-	    for (int y = 0; y < map[x].length; y++) {
-		Tile tile = map[x][y];
-		Texture tex = TileRenderMap.getTexture(tile);
-		if(tex != null)
-		g2d.drawImage(tex.getImage(), x * pixelSize, y * pixelSize, pixelSize, pixelSize, tex);
+	if (this.map == null || !map.equals(mapCache)) {
+	    this.map = new BufferedImage(mapgen.getMapWidth() * tileSize, mapgen.getMapHeight() * tileSize, BufferedImage.TYPE_INT_ARGB);
+	    Graphics2D g = this.map.createGraphics();
+	    for (int x = 0; x < map.length; x++) {
+		for (int y = 0; y < map[x].length; y++) {
+		    Tile tile = map[x][y];
+		    Texture tex = TileRenderMap.getTexture(tile);
+		    if (tex == null)
+			continue;
+		    g.drawImage(tex.getImage(), x * tileSize, y * tileSize, tileSize, tileSize, tex);
+		}
 	    }
+	    g.dispose();
+	    mapCache = map;
 	}
-	// Buildings
-	for (int x = 0; x < map.length; x++) {
-	    for (int y = 0; y < map[x].length; y++) {
+	g2d.drawImage(this.map, 0, 0, null);
+
+	Building[][] buildings = mapgen.getBuildings();
+	for (int x = 0; x < buildings.length; x++) {
+	    for (int y = 0; y < buildings[x].length; y++) {
 		if (buildings[x][y] != null) {
 		    Texture tex = buildings[x][y].getTexture();
-		    if(tex != null)
-		    g2d.drawImage(tex.getImage(), x * pixelSize, y * pixelSize, tex.getWidth()/(pixelSizeBig/pixelSize), tex.getHeight()/(pixelSizeBig/pixelSize), tex);
+		    if (tex != null)
+			g2d.drawImage(tex.getImage(), x * tileSize, y * tileSize, tex.getWidth() / (TILE_SIZE_NORMAL / tileSize), tex.getHeight() / (TILE_SIZE_NORMAL / tileSize), tex);
 		}
 	    }
 	}
